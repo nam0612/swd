97cae8e0fbc24302263daa356bbd0895cc01894c
Saka289 <saka289@icloud.com>
2023-11-06 14:06:35 +0700

    udate

--- src/main/java/com/fpt/swd/database/dto/ClassStudent/GetClassStudentDto.java
+++ src/main/java/com/fpt/swd/database/dto/ClassStudent/GetClassStudentDto.java
@@ -17,19 +17,5 @@ public class GetClassStudentDto implements Serializable {
     private byte status;
     private String created_by;
     private String updated_by;
-    private String className;
-    private String userFirstName;
-    private String userLastName;
 
-    public GetClassStudentDto(int id, int class_id, int student_id, byte status, String created_by, String updated_by, String className, String userFirstName, String userLastName) {
-        this.id = id;
-        this.class_id = class_id;
-        this.student_id = student_id;
-        this.status = status;
-        this.created_by = created_by;
-        this.updated_by = updated_by;
-        this.className = className;
-        this.userFirstName = userFirstName;
-        this.userLastName = userLastName;
-    }
 }
